dcd0284970050d3a336b433fb8d6014f63578fdc
Angelo Ashmore <angeloashmore@users.noreply.github.com>
2021-01-31 17:16:51 -1000

    feat: add well-known cookie name

---
+++ src/cookie.ts
@@ -0,0 +1,5 @@
+/**
+ * The well-known name of the cookie used to save the current preview session's
+ * ref.
+ */
+export const preview = 'io.prismic.preview'
--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,7 @@ export * from './defaultEndpoint'
 
 export * as predicate from './predicate'
 
+export * as cookie from './cookie'
+
 export * from './types'
 export * as Response from './types-response'
